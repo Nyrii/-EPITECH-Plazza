5756f597dff9d36deb50146a3d39c4f95d958049
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-04-05 23:34:36 +0200

    IPipe, NamedPipe implemented. Tests have yet do be done

---
+++ nyrii/main.cpp
@@ -0,0 +1,16 @@
+//
+// main.cpp for main.cpp in /Users/noboud_n/rendu/cpp_plazza/nyrii/
+//
+// Made by Nyrandone Noboud-Inpeng
+// Login   <noboud_n@epitech.eu>
+//
+// Started on  Tue Apr  5 21:17:23 2016 Nyrandone Noboud-Inpeng
+// Last update Tue Apr  5 22:13:22 2016 Nyrandone Noboud-Inpeng
+//
+
+#include "Pipe.hpp"
+
+int	main()
+{
+  return (0);
+}
