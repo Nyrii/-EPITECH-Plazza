fe2c8cb2aa9fc437378b9d1265acd8c0c6f19165
Saursinet <florian.saurs@epitech.eu>
2016-04-05 16:34:50 +0200

    okay for ip adress

--- Saursinet/src/Parsing.cpp
+++ Saursinet/src/Parsing.cpp
@@ -5,7 +5,7 @@
 // Login   <saurs_f@epitech.net>
 //
 // Started on  Tue Apr  5 00:10:23 2016 Florian Saurs
-// Last update Tue Apr  5 15:08:04 2016 Florian Saurs
+// Last update Tue Apr  5 16:33:16 2016 Florian Saurs
 //
 
 #include <iostream>
@@ -19,7 +19,7 @@ Parsing::Parsing()
   _reg = new boost::regex[3];
   _reg[0] = boost::regex("0[1-9]([ ]?[0-9]{2}){4}");
   _reg[1] = boost::regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\\.[a-z]{2,4}");
-  _reg[2] = boost::regex("([0-9]{1,3}\\.){3}[0-9]{1,3}");
+  _reg[2] = boost::regex("(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])");
   _nbReg = 3;
 }
 
