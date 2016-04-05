b8a32847efb5a06a206d29e8b0ee0d4ae40c5326
Guillaume Wilmot <guillaume.wilmot@epitech.eu>
2016-04-05 14:12:48 +0200

    Exo 7 consumer producer

--- TestHaineux/src/Main.cpp
+++ TestHaineux/src/Main.cpp
@@ -5,12 +5,14 @@
 // Login   <wilmot_g@epitech.net>
 //
 // Started on  Mon Apr  4 16:01:55 2016 guillaume wilmot
-// Last update Mon Apr  4 22:02:29 2016 guillaume wilmot
+// Last update Tue Apr  5 14:09:49 2016 guillaume wilmot
 //
 
 #include <stdlib.h>
 #include <pthread.h>
 #include <iostream>
+#include <unistd.h>
+#include <signal.h>
 #include "Thread.hpp"
 #include "Mutex.hpp"
 #include "ScopedLock.hpp"
@@ -18,44 +20,64 @@
 
 SafeQueue		test;
 
-void			*push(void *)
+void			*producer(void *)
 {
-  test.push(rand() % 100);
-  return (NULL);
+  while (42)
+    {
+      test.push(rand() % 100);
+      usleep(rand() % 100000);
+      if (test.isFinished())
+	return (NULL);
+    }
 }
 
-void			*pop(void *)
+void			*consumer(void *)
 {
   int			i;
 
-  if (!test.tryPop(&i))
-    std::cout << "false" << std::endl;
-  else
-    std::cout << i << std::endl;
-  return (NULL);
+  while (42)
+    {
+      if (!test.tryPop(&i))
+	{
+	  std::cout << "false" << std::endl;
+	  usleep(rand() % 100000);
+	}
+      else
+	std::cout << i << std::endl;
+      if (test.isFinished())
+	return (NULL);
+    }
+}
+
+void			stop(int)
+{
+  std::cout << "Killed by signal" << std::endl;
+  test.setFinished();
 }
 
 int			main(int ac, char **av)
 {
-  Thread		*thread;
-  int			nbThread;
+  Thread		*Consumers;
+  Thread		*Producers;
+  int			nbConsumer;
+  int			nbProducer;
   int			i;
 
   if (ac < 2)
     return (-1);
-  nbThread = atoi(av[1]);
-  thread = new Thread[nbThread * 2];
-  for (i = 0; i < nbThread / 2; i++)
-    thread[i].start(&push, NULL);
-  test.setFinished();
-  while (i < nbThread)
-    {
-      thread[i].start(&push, NULL);
-      i++;
-    }
-  for (i = 0; i < nbThread; i++)
-    thread[nbThread + i].start(&pop, NULL);
-  for (i = 0; i < nbThread * 2; i++)
-    thread[i].join();
-  delete[] thread;
+  signal(SIGINT, &stop);
+  nbConsumer = atoi(av[1]);
+  nbProducer = atoi(av[2]);
+  Consumers = new Thread[nbConsumer];
+  Producers= new Thread[nbProducer];
+  for (i = 0; i < nbConsumer; i++)
+    Consumers[i].start(&consumer, NULL);
+  for (i = 0; i < nbProducer; i++)
+    Producers[i].start(&producer, NULL);
+  for (i = 0; i < nbConsumer; i++)
+    Consumers[i].join();
+  for (i = 0; i < nbProducer; i++)
+    Producers[i].join();
+  delete[] Consumers;
+  delete[] Producers;
 }
