388e60cd968f9c89f7cf7275efb9a84709543f2d
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-04-05 11:12:13 +0200

    regex updated

--- Saursinet/src/Parsing.cpp
+++ Saursinet/src/Parsing.cpp
@@ -5,7 +5,7 @@
 // Login   <saurs_f@epitech.net>
 //
 // Started on  Tue Apr  5 00:10:23 2016 Florian Saurs
-// Last update Tue Apr  5 02:41:18 2016 Florian Saurs
+// Last update Tue Apr  5 11:12:01 2016 Nyrandone Noboud-Inpeng
 //
 
 #include <iostream>
@@ -17,7 +17,7 @@
 Parsing::Parsing()
 {
   _reg = new boost::regex[3];
-  _reg[0] = boost::regex("0[1-68]([ ]?[0-9]{2}){4}");
+  _reg[0] = boost::regex("0[1-9]([ ]?[0-9]{2}){4}");
   _reg[1] = boost::regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\\.[a-z]{2,4}");
   _reg[2] = boost::regex("([0-9]{1,3}\\.){4}");
   _nbReg = 3;
