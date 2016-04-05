1efdf2f7fe78b4981536b78ab0e2bc15782be206
Saursinet <florian.saurs@epitech.eu>
2016-04-05 15:40:54 +0200

    oaky for parse

--- Saursinet/src/MainPars.cpp
+++ Saursinet/src/MainPars.cpp
@@ -5,7 +5,7 @@
 // Login   <saurs_f@epitech.net>
 //
 // Started on  Tue Apr  5 00:23:53 2016 Florian Saurs
-// Last update Tue Apr  5 02:15:33 2016 Florian Saurs
+// Last update Tue Apr  5 15:40:14 2016 Florian Saurs
 //
 
 #include "../inc/Parsing.hpp"
@@ -15,7 +15,7 @@ int	main(int ac, char **av)
   Parsing	pars;
   int		i;
 
-  i = 0;
+  i = 1;
   if (ac == 1)
     std::cout << "I want the file to parse in parameter" << std::endl;
   else
