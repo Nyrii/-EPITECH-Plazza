9748719e9cd4b14c8c5a73ced77ae6b1591dcfeb
Saursinet <florian.saurs@epitech.eu>
2016-04-05 12:38:48 +0200

    maybe okay for parse

--- Saursinet/src/Parsing.cpp
+++ Saursinet/src/Parsing.cpp
@@ -5,7 +5,7 @@
 // Login   <saurs_f@epitech.net>
 //
 // Started on  Tue Apr  5 00:10:23 2016 Florian Saurs
-// Last update Tue Apr  5 11:12:01 2016 Nyrandone Noboud-Inpeng
+// Last update Tue Apr  5 12:36:37 2016 Florian Saurs
 //
 
 #include <iostream>
@@ -17,7 +17,7 @@
 Parsing::Parsing()
 {
   _reg = new boost::regex[3];
-  _reg[0] = boost::regex("0[1-9]([ ]?[0-9]{2}){4}");
+  _reg[0] = boost::regex("0[1-68]([ ]?[0-9]{2}){4}");
   _reg[1] = boost::regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\\.[a-z]{2,4}");
   _reg[2] = boost::regex("([0-9]{1,3}\\.){4}");
   _nbReg = 3;
@@ -42,28 +42,42 @@ std::string	Parsing::searchInCurrent(std::string current) const
 void		Parsing::parseFile(std::string name) const
 {
   std::string	result("");
-  std::string	current;
+  std::string	current("");
+  std::string	tmp("");
+  std::vector<std::string>	found;
   DIR		*directory;
 
   directory = opendir(name.c_str());
   if (directory != NULL)
     {
+      closedir(directory);
       std::cerr << name << " is a directory." << std::endl;
       return ;
     }
-  closedir(directory);
   std::ifstream	file(name.c_str(), std::ifstream::in);
 
-  if (file.is_open())
-    {
-      while (!file.eof() && result == "")
-	{
-	  std::getline(file, current);
-	  result = searchInCurrent(current);
-	}
-    }
-  if (result != "")
-    std::cout << result << std::endl;
+  std::string content( (std::istreambuf_iterator<char>(file) ),
+                       (std::istreambuf_iterator<char>()    ) );
+  // if (file.is_open())
+  //   {
+  //     while (!file.eof())
+  // 	{
+  // 	  std::getline(file, current);
+  // 	  tmp += current;
+  // 	}
+  //   }
+  // while (content != "")
+  //   {
+      // std::cout << "boucle inf" << content << std::endl;
+  //     exit(0);
+  result = searchInCurrent(content);
+  found.push_back(result);
+      // content = content.substr(content.find(result));
+    //   exit(0);
+    // }
+  if (found.size() != 0)
+    for (unsigned int i = 0; i < found.size(); ++i)
+      std::cout << found.at(i) << std::endl;
   else
     std::cout << "Not found" << std::endl;
   file.close();
