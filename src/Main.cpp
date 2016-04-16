c247329d3c5211470585d89301a8b367e40439b3
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-04-16 18:56:00 +0200

    main modified

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,7 +5,7 @@
 // Login   <noboud_n@epitech.eu>
 //
 // Started on  Tue Apr  5 21:17:23 2016 Nyrandone Noboud-Inpeng
-// Last update Sat Apr 16 17:45:19 2016 Nyrandone Noboud-Inpeng
+// Last update Sat Apr 16 18:55:50 2016 Nyrandone Noboud-Inpeng
 //
 
 #include <iostream>
@@ -20,48 +20,28 @@
 
 int		main(int, char **)
 {
-  Pipes		tmp(1);
-  t_processState mdr;
+  int	nbThreads;
 
-  mdr.id = 1;
-  if (fork() == 0)
-    {
-      tmp.write(mdr);
-      exit(0);
+  nbThreads = 2;
+  if (ac == 2)
+    try {
+      nbThreads = std::stoi(av[1]);
+    } catch (std::invalid_argument &e) {
+      std::cerr << e.what() << ": invalid argument, positive number expected."<< std::endl;
+      return (-1);
     }
-  mdr.id = 9999;
-  tmp.read(mdr);
-  std::cout << mdr.id << std::endl;
-  mdr.id = 1;
-  if (fork() == 0)
+  if (nbThreads <= 0)
     {
-      tmp.write(mdr);
-      exit(0);
+      std::cerr << "Usage: ./plazza [nbr of threads]" << std::endl << "Positive non-NULL number expected." << std::endl;
+      return (-1);
     }
-  tmp.read(mdr);
-  std::cout << mdr.id << std::endl;
-  // int	nbThreads;
-  //
-  // nbThreads = 2;
-  // if (ac == 2)
-  //   try {
-  //     nbThreads = std::stoi(av[1]);
-  //   } catch (std::invalid_argument &e) {
-  //     std::cerr << e.what() << ": invalid argument, positive number expected."<< std::endl;
-  //     return (-1);
-  //   }
-  // if (nbThreads <= 0)
-  //   {
-  //     std::cerr << "Usage: ./plazza [nbr of threads]" << std::endl << "Positive non-NULL number expected." << std::endl;
-  //     return (-1);
-  //   }
-  // Core	*core = new Core(nbThreads);
-  //
-  //   try {
-  //     core->read();
-  //   } catch (Error &e) {
-  //     std::cerr << e.what() << std::endl;
-  //   }
-  // delete core;
-  // return (0);
+  Core	*core = new Core(nbThreads);
+
+    try {
+      core->read();
+    } catch (Error &e) {
+      std::cerr << e.what() << std::endl;
+    }
+  delete core;
+  return (0);
 }
