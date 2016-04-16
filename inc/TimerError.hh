fe4b4cb9fa15c0005571e7821fdaee68f714a3a4
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-04-16 20:27:35 +0200

    TimerError implemented : it now compile

--- inc/TimerError.hh
+++ inc/TimerError.hh
@@ -5,11 +5,13 @@
 // Login   <noboud_n@epitech.eu>
 //
 // Started on  Sat Apr 16 20:01:33 2016 Nyrandone Noboud-Inpeng
-// Last update Sat Apr 16 20:02:05 2016 Nyrandone Noboud-Inpeng
+// Last update Sat Apr 16 20:27:09 2016 Nyrandone Noboud-Inpeng
 //
 
 #ifndef TIMERERROR_HH_
 # define TIMERERROR_HH_
+
+# include "Error.hpp"
 
 class			TimerError : public Error
 {
